519057d2aeb9cd854c8a1fc89c3253d2f96bcf5a
Sztorm <marcinwicher99@gmail.com>
2021-03-16 11:55:03 +0100

    added book.

---
+++ Book.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Zadanie 
+{
+    public class Book
+    {
+        public string Title { get; }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+    }
+}
