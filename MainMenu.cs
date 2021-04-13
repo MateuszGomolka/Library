53d3674a04a0a97beaa35a7287363126972a3d85
Sztorm <marcinwicher99@gmail.com>
2021-04-13 10:43:28 +0200

    Added MainMenu class with base structure.

---
+++ MainMenu.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Zadanie 
+{
+    public class MainMenu
+    {
+        public void AddBook()
+        {
+
+        }
+
+        public void FindBookByTitle()
+        {
+
+        }
+
+        public void FindBookByAuthor()
+        {
+            
+        }
+    }
+}
